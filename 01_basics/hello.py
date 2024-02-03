e34834b3fd1e24f648d975e3963043a1ace69573
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-03 08:56:29 +0000

    Python Basics

---
+++ 01_basics/hello.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+def name(n):
+ print("Hello my name is " + n)
+ 
+name("Devawayan")
+
+name_one = "Divyansh Gandu"
+name_two = "Pratyush"
+name_three = "Sushain"
--- Python/Exercise 2.py
+++
@@ -1,3 +0,0 @@
-superhero= input("Enter Your Superhero Name: ")
-
-print("This is the Superhero Tony Entered : " + superhero)
