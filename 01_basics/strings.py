79e4eec2f67fe1cbfccc00d56540ac9155e1acb6
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-16 16:44:19 +0000

    Python

---
+++ 01_basics/strings.py
@@ -0,0 +1,39 @@
+print("Hello")
+
+print('Hello World')
+
+print("""Don't be scaared of this language""")
+
+name = "Devawayan"
+
+#Slicing in Character
+
+print(name)
+
+result = name[0:3]
+
+print(result)
+
+#Slicing in Number
+
+num_list = [1,2,3,4,5,6]
+
+sliced_list = num_list[0:4]
+
+print(sliced_list)
+
+#different types of slicing.
+
+# number_character = "0123456789"
+# >>> number_character[:]
+# '0123456789'
+# >>> number_character[3:6]
+# '345'
+# >>> number_character[3:]
+# '3456789'
+# >>> number_character[:8]
+# '01234567'
+# >>> number_character[0:8:1]
+# '01234567'
+# >>> number_character[0:8:3]
+# '036'
