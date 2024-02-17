6e4223c288a4f8a0dba3e8543e665fe5c9c2bd36
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-17 22:06:37 +0000

    Python Learning

---
+++ 01_basics/strings2.py
@@ -0,0 +1,28 @@
+name = "Devawayan"
+
+print(len(name))
+
+for letter in name:
+    print(letter)
+
+#In this the \ sign is used to make the String as it is Raw String
+
+#Method 1:
+
+chai = "He said, \"Masala Chai is awesome\" "
+print(chai)
+
+path_one = "c:\\users\\pwd\\desktop"
+print(path_one)
+
+#Method 2:
+#to write an r sign before string so it consider it at raw string.
+
+name = r'My name is Devawayan, "I love my Parents more than anyone"'
+print(name)
+
+path_two = r"c:\users\pwd\desktop"
+print(path_two)
+
+chai = "Ginger Tea"
+print("Ginger" in chai)
