dacae8310f1c69d88360cc46abd96df40b44ebe3
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-17 10:43:52 +0000

    Python_Basics

---
+++ 01_basics/replace.py
@@ -0,0 +1,13 @@
+chai = "Masala Tea"
+
+print(chai)
+
+updated = chai.replace("Masala" , "Lemon")
+
+print(updated)
+
+#Stripping Method for removing extra spaces.
+
+abc = "  Devawayan  "
+
+print(abc.strip())
