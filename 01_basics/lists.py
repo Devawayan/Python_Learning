09710819285e88133c2ca125a9d49e5c419a40ab
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-17 22:31:38 +0000

    Python Learning

---
+++ 01_basics/lists.py
@@ -0,0 +1,26 @@
+tea_varities = ["Black Tea" , "Green Tea" , "Oolong Tea" , "White Tea"]
+
+print(tea_varities)
+
+#Different Techniques in Lists that can be performed:
+
+#find: 
+
+print(tea_varities[1])
+
+#Slicing
+
+print(tea_varities[0:2])
+
+#replace
+
+tea_varities[3] = "Herbal Tea"
+
+print(tea_varities)
+
+tea_varities[1:2] = ["Lemon Tea"]
+print(tea_varities)
+
+tea_varities[1:3] = ["Green Tea" , "Masala Tea"]
+print(tea_varities)
+
