3b4fa0b01a722c9f1730f12f74438e8277517e26
Devawayan Hore <87519958+Devawayan@users.noreply.github.com>
2024-02-17 21:40:31 +0000

    Python Learning

--- 01_basics/replace.py
+++ 01_basics/replace.py
@@ -11,3 +11,30 @@ print(updated)
 abc = "  Devawayan  "
 
 print(abc.strip())
+
+#Spliting Techique in Python.
+#Using this technique you can convert string to list.
+
+name = "Dev , Divyansh , Pratyush"
+print(name)
+print(name.split(", "))
+
+name_one = "Dev"
+print(name_one.find("D"))
+
+repeat_name = "Dev Dev Devawayan Dev"
+print(repeat_name.count("Dev"))
+
+#in this between the string if you add {} it is known as placeholders and it is used to add Variables.
+
+chai_type = "Masala"
+quantity = 2
+order = "I ordered {} cups of {} chai"
+
+print(order.format(quantity , chai_type))
+
+#In this the join opreation is performed to convert list into string
+#in it the "" are given usually to match the gap bydefault.
+
+chai_variety = ["Lemon" , "Masala" , "Ginger"]
+print(", ".join(chai_variety))
